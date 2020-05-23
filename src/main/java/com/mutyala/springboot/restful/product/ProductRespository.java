500ce376ea0793cfce1e8e378bfbe8bd1c77d97a
shrikant_k_mutyala <shrikantmutyala@gmail.com>
2020-05-23 19:53:51 +0530

    springboot-swagger

---
+++ src/main/java/com/mutyala/springboot/restful/product/ProductRespository.java
@@ -0,0 +1,6 @@
+package com.mutyala.springboot.restful.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRespository extends JpaRepository<Product, Long> {
+}
---
+++ src/main/java/com/mutyala/springboot/restful/product/ProductService.java
@@ -0,0 +1,31 @@
+package com.mutyala.springboot.restful.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+
+@Service
+public class ProductService {
+	
+    private final ProductRespository productRespository;
+
+    public List<Product> findAll() {
+        return productRespository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRespository.findById(id);
+    }
+
+    public Product save(Product stock) {
+        return productRespository.save(stock);
+    }
+
+    public void deleteById(Long id) {
+        productRespository.deleteById(id);
+    }
+}
